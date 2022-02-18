3a136fe198ce516dbb32b1ce0d69a82a06e7147f
selcuk cabuk <cabuk.selcuk@gmail.com>
2022-02-18 09:45:53 +0300

    add initial files

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.scss"
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            Hello From App
+        </div>
+    )
+}
+
+export default App;
