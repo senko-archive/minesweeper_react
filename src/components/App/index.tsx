ce7cd9b7ad9910ba8436b5e66c4071813b7a8035
selcuk cabuk <cabuk.selcuk@gmail.com>
2022-02-18 12:16:05 +0300

    build main structure

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import NumberDisplay from "../NumberDisplay"
 
 import "./App.scss"
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            Hello From App
+            <div className="Header">
+                <NumberDisplay value={0}></NumberDisplay>
+                <div className="Face">
+                    <span role="img" aria-label="face">
+                        😄
+                    </span>
+                </div>
+                <NumberDisplay value={23}></NumberDisplay>
+            </div>
+            <div className="Body">Body</div>
         </div>
     )
 }
